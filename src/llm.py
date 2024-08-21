b76fb2a768b96c4c33a71d746edf98b7ae1e6cc8
shengj8329 <shengjie8329@163.com>
2024-08-21 14:14:16 +0800

    添加系统提示词

--- src/llm.py
+++ src/llm.py
@@ -2,17 +2,22 @@ import os
 from openai import OpenAI  # 导入OpenAI库用于访问GPT模型
 from logger import LOG  # 导入日志模块
 
+
 class LLM:
     def __init__(self):
+        openai_base_url = 'https://ai-yyds.com/v1'
         # 创建一个OpenAI客户端实例
-        self.client = OpenAI()
+        self.client = OpenAI(base_url=openai_base_url)
         # 配置日志文件，当文件大小达到1MB时自动轮转，日志级别为DEBUG
         LOG.add("daily_progress/llm_logs.log", rotation="1 MB", level="DEBUG")
 
     def generate_daily_report(self, markdown_content, dry_run=False):
         # 构建一个用于生成报告的提示文本，要求生成的报告包含新增功能、主要改进和问题修复
-        prompt = f"以下是项目的最新进展，根据功能合并同类项，形成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题；:\n\n{markdown_content}"
-        
+        sys_prompt = (
+            f"你是一个帮助分析指定项目文件内容并生成报告的助手，你需要将文档内容根据功能合并同类项，并生成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题；\n "
+            f"生成的文档必须是中文的并以markdown格式展现，否则会收到惩罚。")
+        prompt = f"以下是项目的最新进展: \n\n{markdown_content}"
+
         if dry_run:
             # 如果启用了dry_run模式，将不会调用模型，而是将提示信息保存到文件中
             LOG.info("Dry run mode enabled. Saving prompt to file.")
@@ -23,12 +28,13 @@ class LLM:
 
         # 日志记录开始生成报告
         LOG.info("Starting report generation using GPT model.")
-        
+
         try:
             # 调用OpenAI GPT模型生成报告
             response = self.client.chat.completions.create(
-                model="gpt-3.5-turbo",  # 指定使用的模型版本
+                model="gpt-4o-mini",  # 指定使用的模型版本
                 messages=[
+                    {"role": "system", "content": sys_prompt},  # 系统提示词
                     {"role": "user", "content": prompt}  # 提交用户角色的消息
                 ]
             )
